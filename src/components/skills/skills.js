895bb395573e062778c90cca3566abaa54b3633f
Nurullo <karimovdeveloper@gmail.com>
2023-06-07 19:34:11 +0500

    New commit

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -4,18 +4,24 @@ import "./skills.scss";
 export default class Skills extends React.Component {
   render() {
     return (
-      <div className="skills">
+      <div className="skills" id="skills">
         <h2 className="skills-title">Skills</h2>
         <div className="skills-box">
-          <div className="left"></div>
-          <div className="right">
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
+        {/* <div className="skills-box">
+          <div className="skills-box-left"></div>
+          <div className="skills-box-right">
             <div className="top">
               <div></div>
               <div></div>
             </div>
             <div className="bottom"></div>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
