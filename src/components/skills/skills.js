166f79d1b617ba82299252fdce56ab3402827596
Nurullo <karimovdeveloper@gmail.com>
2023-06-07 19:08:37 +0500

    New commit

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -6,6 +6,16 @@ export default class Skills extends React.Component {
     return (
       <div className="skills">
         <h2 className="skills-title">Skills</h2>
+        <div className="skills-box">
+          <div className="left"></div>
+          <div className="right">
+            <div className="top">
+              <div></div>
+              <div></div>
+            </div>
+            <div className="bottom"></div>
+          </div>
+        </div>
       </div>
     );
   }
