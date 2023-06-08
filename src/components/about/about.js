da81792f6d107508e274b8c00b6e6f3826870265
Nurullo <karimovdeveloper@gmail.com>
2023-06-08 12:20:06 +0500

    New commit

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,7 +6,27 @@ export default class About extends React.Component {
     return (
       <div className="about" id="about">
         <div className="about-left">
-          <div className="about-title"></div>
+          <h2 className="about-title">About Us</h2>
+          <p>
+            The model offers a framework for discussing problems related to the user's experience, as well as possible
+            ways and means of solving them. Application development begins at the top level (strategy), where the future
+            software product is described quite abstractly from the point of view of the expectations of both users and
+            the customer.
+          </p>
+          <div className="static">
+            <div className="item">
+              <span>800</span>
+              <p>800</p>
+            </div>
+            <div className="item">
+              <span>18</span>
+              <p>Teachers</p>
+            </div>
+            <div className="item">
+              <span>6</span>
+              <p>Foreign languages</p>
+            </div>
+          </div>
         </div>
         <div className="about-right"></div>
       </div>
