69fda0856293f3f91313759501fdba2477655cc4
Nurullo <karimovdeveloper@gmail.com>
2023-06-08 13:31:45 +0500

    New commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Header from "./components/header/header";
 import Home from "./components/home/home";
 import Skills from "./components/skills/skills";
 import About from "./components/about/about";
+import Contact from "./components/contact/contact";
 export default class App extends React.Component {
   render() {
     return (
@@ -11,6 +12,7 @@ export default class App extends React.Component {
         <Home />
         <Skills />
         <About />
+        <Contact />
       </div>
     );
   }
